6f0ab6110fda76c4fe3496afedf151e7f007d386
divya687304 <125003450@sastra.ac.in>
2024-06-21 12:17:55 +0530

    assignment 2

---
+++ assignment2/exceptions/main.cpp
@@ -0,0 +1,14 @@
+#include "solution.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+        int num;
+        cin>>num;
+        double result = computeSquareRoot(num);
+
+     
+        cout << "The square root of " << num << " is " << result << std::endl;
+   
+}
+
